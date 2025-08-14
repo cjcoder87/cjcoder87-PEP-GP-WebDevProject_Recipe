544d79b81d088e235a57d899c0158f8f0be3bab3
Christopher Jackson <c.jackson629@gmail.com>
2025-08-14 20:39:53 +0000

    re add base Url

--- src/main/resources/public/frontend/recipe/recipe-page.js
+++ src/main/resources/public/frontend/recipe/recipe-page.js
@@ -2,7 +2,7 @@
  * This script defines the CRUD operations for Recipe objects in the Recipe Management Application.
  */
 
-"http://localhost:8081"; // backend URL
+const BASE_URL = "http://localhost:8081"; // backend URL
 
 let recipes = [];
 
